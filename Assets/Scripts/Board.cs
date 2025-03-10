c3ae7a7f2bf0f734a1df000a5ba3dc99c60326cc
Joseph <sourpina@gmail.com>
2025-03-10 02:51:50 -0400

    Implemented Constructor and ReplaceCards

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -20,7 +20,9 @@ public class Board
 
     public Board()
     {
-
+        Deck = new Deck();
+        Deck.Shuffle();
+        TableCards = new List<Card>(9);
     }
 
     /// <summary>
@@ -32,6 +34,33 @@ public class Board
     /// <param name="index3">[Optional] The table index of the third card to replace</param>
     public void ReplaceCards(int index1, int index2, int index3 = -1)
     {
+        bool triplet = index3 != -1;
 
+        // Remove cards from table
+        TableCards.RemoveAt(index1);
+        TableCards.RemoveAt(index2);
+        if (triplet) 
+            TableCards.RemoveAt(index3);
+
+        // Replace cards with top card from Deck
+        Card newCard = Deck.TakeTopCard();
+        if (newCard == null)
+            return;
+        TableCards.Insert(index1, newCard);
+
+        newCard = Deck.TakeTopCard();
+        if (newCard == null)
+            return;
+        TableCards.Insert(index2, newCard);
+
+        
+        if (!triplet)
+            return;
+
+        // Replace third card if needed
+        newCard = Deck.TakeTopCard();
+        if (newCard == null)
+            return;
+        TableCards.Insert(index3, newCard);
     }
 }
