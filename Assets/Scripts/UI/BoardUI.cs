92900e36fa1314df68c42c0e8f328eca68b49259
Joseph Collado <sourpina@gmail.com>
2025-03-14 00:57:15 -0400

    Changed SetBoardLayout to not use fixed columns
    Instead SetBoardLayout tries to make a layout that's as square as possible

--- Assets/Scripts/UI/BoardUI.cs
+++ Assets/Scripts/UI/BoardUI.cs
@@ -1,6 +1,5 @@
 using UnityEngine;
 using UnityEngine.UI;
-using System.Collections.Generic;
 
 public class BoardUI : MonoBehaviour
 {
@@ -24,18 +23,19 @@ public class BoardUI : MonoBehaviour
     }
 
     /// <summary>
-    /// Sets the board's GridLayoutGroup padding based on the board size with a fixed column size of 5
+    /// Sets the board's GridLayoutGroup padding based on the board size
     /// </summary>
     /// <param name="boardSize">The maximum number of cards that will be on the board</param>
     private void SetBoardLayout(int boardSize)
     {
-        int columns = 5;
-        int rows = boardSize / 5;
+        int columns = Mathf.CeilToInt(Mathf.Sqrt(boardSize));
+        int rows = Mathf.CeilToInt((float)boardSize / columns);
 
         GridLayoutGroup glg = GetComponent<GridLayoutGroup>();
         RectTransform rectTransform = GetComponent<RectTransform>();
-        glg.padding.top = (int)((rectTransform.sizeDelta.y - glg.cellSize.y * rows - glg.spacing.y * (rows + 1)) / 2);
-        glg.padding.left = (int)((rectTransform.sizeDelta.x - glg.cellSize.x * columns - glg.spacing.x * (columns + 1)) / 2);
+        glg.constraintCount = columns;
+        glg.padding.top = (int)((rectTransform.sizeDelta.y - glg.cellSize.y * rows - glg.spacing.y * (rows - 1)) / 2);
+        glg.padding.left = (int)((rectTransform.sizeDelta.x - glg.cellSize.x * columns - glg.spacing.x * (columns - 1)) / 2);
     }
 
     #endregion
