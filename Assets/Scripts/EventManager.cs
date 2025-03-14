3719d58a54a07a08c7677f089fe5724e6922203a
Joseph Collado <sourpina@gmail.com>
2025-03-14 01:33:14 -0400

    Added Game Set Up event
    Also renamed the GameStarted event to GameStarting to better reflect the action

--- Assets/Scripts/EventManager.cs
+++ Assets/Scripts/EventManager.cs
@@ -3,6 +3,10 @@ using System;
 public static class EventManager
 {
     // Start Game Event
-    public static event Action GameStarted;
-    public static void StartGame() => GameStarted?.Invoke();
+    public static event Action GameStarting;
+    public static void StartGame() => GameStarting?.Invoke();
+
+    // Set Up Game Event
+    public static event Action<int> GameSettingUp;
+    public static void SetUpGame(int boardSize) => GameSettingUp?.Invoke(boardSize);
 }
