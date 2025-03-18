ec0c86acd85a0e1dc49d067040a821877a65cbb6
Joseph Collado <sourpina@gmail.com>
2025-03-18 14:12:28 -0400

    Added display update functionality

--- Assets/Scripts/UI/GameUI.cs
+++ Assets/Scripts/UI/GameUI.cs
@@ -5,9 +5,15 @@ public class GameUI : MonoBehaviour
 {
     [SerializeField] private GameObject _resultPanel;
     [SerializeField] private TextMeshProUGUI _resultText;
+    [SerializeField] private TextMeshProUGUI _gamesWonText;
+    [SerializeField] private TextMeshProUGUI _gamesPlayedText;
+    [SerializeField] private TextMeshProUGUI _winRateText;
 
     private const string LOSS_TEXT = "No more moves remaining!\nYou lost!";
     private const string WIN_TEXT = "Board cleared!\nCongratulations!\n You won!";
+    private const string GAMES_WON_TEXT = "Wins: ";
+    private const string GAMES_PLAYED_TEXT = "Played: ";
+    private const string WIN_RATE_TEXT = "Win Rate: ";
 
     #region Monobehaviour Callbacks
 
@@ -15,12 +21,14 @@ public class GameUI : MonoBehaviour
     {
         EventManager.GameLost += OnLose;
         EventManager.GameWon += OnWin;
+        EventManager.UpdateDisplay += OnUpdateDisplay;
     }
 
     private void OnDisable()
     {
         EventManager.GameLost -= OnLose;
         EventManager.GameWon -= OnWin;
+        EventManager.UpdateDisplay -= OnUpdateDisplay;
     }
 
     #endregion
@@ -37,6 +45,13 @@ public class GameUI : MonoBehaviour
     {
         _resultPanel.SetActive(true);
         _resultText.text = WIN_TEXT;
+    }
+
+    private void OnUpdateDisplay(int won, int played)
+    {
+        _gamesWonText.text = $"{GAMES_WON_TEXT}{won}";
+        _gamesPlayedText.text = $"{GAMES_PLAYED_TEXT}{played}";
+        _winRateText.text = $"{WIN_RATE_TEXT}{(100f * won / played):F2}%";
     }
 
     #endregion
