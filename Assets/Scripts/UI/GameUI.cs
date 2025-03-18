2ae6f6ce38f8306d0aeabba81df407f0088ed152
Joseph Collado <sourpina@gmail.com>
2025-03-18 11:52:37 -0400

    Added callback to show loss text on lost game

--- Assets/Scripts/UI/GameUI.cs
+++ Assets/Scripts/UI/GameUI.cs
@@ -1,7 +1,41 @@
+using TMPro;
 using UnityEngine;
 
 public class GameUI : MonoBehaviour
 {
-    // Called by UI button press
+    [SerializeField] private GameObject _resultPanel;
+    [SerializeField] private TextMeshProUGUI _resultText;
+
+    private const string LOSS_TEXT = "No more moves remaining!\nYou lost!";
+
+    #region Monobehaviour Callbacks
+
+    private void OnEnable()
+    {
+        EventManager.LostGame += OnLose;
+    }
+
+    private void OnDisable()
+    {
+        EventManager.LostGame -= OnLose;
+    }
+
+    #endregion
+
+    #region Event Callbacks
+
+    private void OnLose()
+    {
+        _resultPanel.SetActive(true);
+        _resultText.text = LOSS_TEXT;
+    }
+
+    #endregion
+
+    #region Button Callbacks
+
     public void OnStartGame() => EventManager.StartGame();
+    
+
+    #endregion
 }
